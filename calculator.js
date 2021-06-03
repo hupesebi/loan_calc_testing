90aba26944aa1b5a703fc96425d678ddcb430414
Sebastian Hupe <Sebastian.Hupe@gmail.com>
2021-06-03 17:13:02 +0200

    Added code to calculaor.js to make it work

--- calculator.js
+++ calculator.js
@@ -21,20 +21,45 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const values = {
+    amount: 50000,
+    years: 10,
+    rate: 10,
+  };
+  let amountField = document.getElementById("loan-amount");
+  amountField.value = values.amount;
+  let yearField = document.getElementById("loan-years");
+  yearField.value = values.years;
+  let rateField = document.getElementById("loan-rate");
+  rateField.value = values.rate;
+  update();
+
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  let currentUIvalues = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(currentUIvalues));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+
+const interest = (values.rate / 100) / 12;
+  const n = values.years * 12;
+  return (
+    (interest * values.amount) /
+    (1 - Math.pow((1 + interest), -n))
+  ).toFixed(2);
+
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const monthlyPayout = document.getElementById("monthly-payment");
+  monthlyPayout.innerText = `You get ${monthly} dollar per month` ;
 }
